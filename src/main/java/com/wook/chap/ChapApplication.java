fe61e3e49a4ed2c8f09db5cda1a5f7a2a8516074
kangwook <rlarkddnr1686@naver.com>
2023-03-21 15:05:39 +0900

    <feat> 프로젝트 생성 및 초기 설정 작업
    * 로컬/개발 두 환경으로 분리하여 개발 진행

---
+++ src/main/java/com/wook/chap/ChapApplication.java
@@ -0,0 +1,13 @@
+package com.wook.chap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChapApplication.class, args);
+	}
+
+}
