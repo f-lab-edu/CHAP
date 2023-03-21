c8797e9eb4c422a80a956f7ad6d56851b8a1af8f
kangwook <rlarkddnr1686@naver.com>
2023-03-21 19:12:32 +0900

    <feat> 스프링 시큐리티에 대한 설정 파일 작성
    * 로그인, 회원가입 페이지를 제외한 나머지 페이지에 대한 접근 시 인증을 요함
    * 그 외에 h2-console, css 파일 등에 대해서 인증을 요하지 않게 설정

---
+++ src/main/java/com/wook/chap/config/SecurityConfig.java
@@ -0,0 +1,74 @@
+package com.wook.chap.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, TokenProvider tokenProvider,
+                                           JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
+                                           JwtAccessDeniedHandler jwtAccessDeniedHandler) throws Exception {
+
+        http
+                .csrf().disable()
+
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+
+                .and()
+                .headers()
+                .frameOptions()
+                .sameOrigin()
+
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
+                .authorizeHttpRequests()
+                .antMatchers(HttpMethod.GET, "/login", "/signUp").permitAll()
+                .anyRequest().authenticated()
+
+                .and()
+                .apply(new JwtSecurityConfig(tokenProvider))
+
+                .and()
+                .logout()
+                .deleteCookies(JwtFilter.AUTHORIZATION_HEADER);
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+
+        // Random한 Salt 값을 받아서 같이 hashing을 이용하는 BCryptPasswordEncoder 사용
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public WebSecurityCustomizer configure() {
+        return (web) -> {
+            web.ignoring().mvcMatchers(
+                    "/h2-console/**",
+                    "/favicon.ico",
+                    "/css/**",
+                    "/js/**",
+                    "/images/**"
+            );
+        };
+    }
+}
